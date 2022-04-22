7a9877747551c9991b2380afd942c7777bd97ac0
Maxime48 <maxime4570@gmail.com>
2022-04-22 09:03:13 +0200

    Adding essential forgotten files to git

---
+++ app/Models/Scrgb_Image_Requests.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Scrgb_Image_Requests extends Model
+{
+    use HasFactory;
+}
