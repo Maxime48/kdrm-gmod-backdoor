680bbf9c4315c363c490b51a1697d717e6c4ebb8
Maxime48 <maxime4570@gmail.com>
2022-05-23 10:16:40 +0200

    Implementation of a better display

--- resources/views/admin/logs.blade.php
+++ resources/views/admin/logs.blade.php
@@ -22,9 +22,9 @@
                 </div>
             </div>
         @endif
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <div class="p-6">
 
                     <div class="container-fluid mt-100">
 
@@ -33,34 +33,30 @@
                             <!--<div class="col-12 col-md-3 p-0 mb-3"> <input type="text" class="form-control" placeholder="Search..."> </div>-->
                         </div>
 
-                        <div class="card mb-3">
-                            <div class="card-header pl-0 pr-0">
-                                <div class="row no-gutters w-110 align-items-center">
-                                    <div class="col ml-2">Message</div>
-                                    <div class="col-10 text-muted">
-                                        <div class="row no-gutters align-items-center">
-                                            <div class="col-3">Level</div>
-                                            <div class="col-3">Date</div>
-                                            <div class="col-2">User</div>
-                                            <div class="col-3">Server</div>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div class="table100 ver1 m-b-110">
+                            <div class="table100-head">
+                                <table>
+                                    <thead>
+                                    <tr class="row100 head">
+                                        <th class="cell100 column1">Message</th>
+                                        <th class="cell100 column2">Level</th>
+                                        <th class="cell100 column3">Date</th>
+                                        <th class="cell100 column4">User</th>
+                                        <th class="cell100 column5">Server</th>
+                                    </tr>
+                                    </thead>
+                                </table>
                             </div>
-
-                            @foreach ($logs as $log)
-                                <hr class="m-0">
-                                <div class="card-body py-3">
-                                    <div class="row no-gutters align-items-center">
-
-                                        <div class="col-2">
-
-                                            {{ \Illuminate\Support\Str::limit($log->message, 650, $end='...') }}
-
-                                        </div>
-
-                                        <div class="d-none d-md-block col-2">
-                                            <div class="alert
+                            <div class="table100-body js-pscroll">
+                                <table>
+                                    <tbody>
+                                    @foreach($logs as $log)
+                                        <tr class="row100 body">
+                                            <td class="cell100 column1">
+                                                {{ \Illuminate\Support\Str::limit($log->message, 650, $end='...') }}
+                                            </td>
+                                            <td class="cell100 column2">
+                                                <div class="alert
                                                             @if($log->level == "notice")
                                                                 alert-info
                                                             @elseif($log->level == "warning")
@@ -77,57 +73,37 @@
                                                                 alert-light
                                                             @endif
                                                         "
-                                                 role="alert">
+                                                     role="alert">
                                                     <div style="text-align: center;">
                                                         {{ $log->level }}
                                                     </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="media col-3 align-items-center">
-                                            <div class="media-body flex-truncate ml-2">
-                                                <div class="line-height-1 text-truncate align-items-center">
-
-                                                    {{ $log->created_at }}
-                                                    <br>
-                                                    {{ adminLogic::time_elapsed_string($log->created_at) }}
-
                                                 </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-2">
-                                            <a style="text-decoration: none;" href="{{ route('showUserProfile', ['id' => $log->user_id]) }}" >
-                                                {{ adminLogic::getUserById($log->user_id)->name }}
-                                            </a>
-                                        </div>
-
-                                        <div class="col-3">
-                                            @if( $log->server != "none" )
-                                                ServerDetected-ImplementDisplay
-                                            @else
-                                                No server related to this log
-                                            @endif
-                                        </div>
-
-
-                                    </div>
-                                </div>
-                        @endforeach
-
-                        <!--
-                        <nav>
-                            <ul class="pagination mb-5">
-                                <li class="page-item disabled"><a class="page-link" href="javascript:void(0)" data-abc="true">«</a></li>
-                                <li class="page-item active"><a class="page-link" href="javascript:void(0)" data-abc="true">1</a></li>
-                                <li class="page-item"><a class="page-link" href="javascript:void(0)" data-abc="true">2</a></li>
-                                <li class="page-item"><a class="page-link" href="javascript:void(0)" data-abc="true">3</a></li>
-                                <li class="page-item"><a class="page-link" href="javascript:void(0)" data-abc="true">»</a></li>
-                            </ul>
-                        </nav>
-                            -->
+                                            </td>
+                                            <td class="cell100 column3">
+                                                {{ $log->created_at }}
+                                                <br>
+                                                {{ adminLogic::time_elapsed_string($log->created_at) }}
+                                            </td>
+                                            <td class="cell100 column4">
+                                                <a style="text-decoration: none;" href="{{ route('showUserProfile', ['id' => $log->user_id]) }}" >
+                                                    {{ adminLogic::getUserById($log->user_id)->name }}
+                                                </a>
+                                            </td>
+                                            <td class="cell100 column5">
+                                                @if( $log->server != "none" )
+                                                    ServerDetected-ImplementDisplay
+                                                @else
+                                                    No server related to this log
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                        <div class="btn-group mx-auto"  role="group">
+
+                        <div class="btn-group mx-auto mt-2"  role="group">
                             @for($i = 1; $i <= $buttons; $i++)
                                 @if($buttons > 30)
                                     @if($i<=8 or ($pageid+5 >= $i and $pageid-5 <= $i) or $i >= ($buttons-8))
