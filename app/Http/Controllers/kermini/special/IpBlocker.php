00176f926873f7730ada54885171370361f76834
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2022-09-08 09:52:02 +0200

    Update IpBlocker.php

--- app/Http/Controllers/kermini/special/IpBlocker.php
+++ app/Http/Controllers/kermini/special/IpBlocker.php
@@ -352,7 +352,7 @@ class IpBlocker extends Controller
             $request->user()->admin != 2
         ){
             return redirect()->back()->with(
-                'status', "Not authorized to edit this."
+                'status', "Not authorized to delete this."
             );
         }
 
